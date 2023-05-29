63174e3b8ebbdf39bda9252c52cef23fda545e67
summereuna <summereuna@gmail.com>
2023-05-29 15:27:14 +0900

    Add: practice useReducer

--- src/hooks/use-basic-input.js
+++ src/hooks/use-basic-input.js
@@ -1,27 +1,47 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialInputState = { value: "", isTouched: false };
+const inputStateReducer = (preState, action) => {
+  if (action.type === "INPUT") {
+    return { value: action.value, isTouched: preState.isTouched };
+    //isTouched는 true로 설정하지 않음: 키를 입력 하는 중에 입력을 마치는 것이 아니기 때문에
+    //따라서 그냥 이전 상태로 설정
+  }
+  if (action.type === "BLUR") {
+    return { isTouched: true, value: preState.value };
+  }
+  if (action.type === "RESET") {
+    return { value: "", isTouched: false };
+  }
+  return {
+    value: "",
+    isTouched: false,
+  };
+};
 
 const useBasicInput = (valueValidate) => {
-  const [enteredValue, setEnteredValue] = useState("");
-  const [isTouched, setIsTouched] = useState(false);
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
 
   const valueChangeHandler = (event) => {
-    setEnteredValue(event.target.value);
+    dispatch({ type: "INPUT", value: event.target.value });
   };
   const valueBlurHandler = () => {
-    setIsTouched(true);
+    dispatch({ type: "BLUR" });
   };
 
   const reset = () => {
-    setEnteredValue("");
-    setIsTouched(false);
+    dispatch({ type: "RESET" });
   };
 
-  const isValueValid = valueValidate(enteredValue);
+  const isValueValid = valueValidate(inputState.value);
 
-  const hasError = !isValueValid && isTouched;
+  const hasError = !isValueValid && inputState.isTouched;
 
   return {
-    enteredValue,
+    value: inputState.value,
     isValueValid,
     hasError,
     valueChangeHandler,

7ce23a86b8ece51a0c2f5dae87b0cc2f02bab8ab
summereuna <summereuna@gmail.com>
2023-05-29 13:46:59 +0900

    Question

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,21 +1,23 @@
 const BasicForm = (props) => {
+  //유효성 검증 및 커스텀 훅 사용하여 BasicForm 작성하기
+
   return (
     <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+      <div className="control-group">
+        <div className="form-control">
+          <label htmlFor="name">First Name</label>
+          <input type="text" id="name" />
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className="form-control">
+          <label htmlFor="name">Last Name</label>
+          <input type="text" id="name" />
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="name">E-Mail Address</label>
+        <input type="text" id="name" />
       </div>
-      <div className='form-actions'>
+      <div className="form-actions">
         <button>Submit</button>
       </div>
     </form>
